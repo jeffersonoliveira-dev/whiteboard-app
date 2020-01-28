e9cfc204dffea1c9f22f754b6a55098390ddbd8c
Jefferson Oliveira <cardbeatt@gmail.com>
2020-01-28 17:51:55 -0300

    checking files

--- drawnings/api.py
+++ drawnings/api.py
@@ -6,8 +6,9 @@ from .serializers import DrawningSerializer
 # Drawning viewset
 class DrawningViewSet(viewsets.ModelViewSet):
     queryset = Drawning.objects.all()
+
     permission_classes = [
-            permissions.AllowAny
-            ]
+        permissions.AllowAny
+        ]
     serializer_class = DrawningSerializer
 
--- drawnings/serializers.py
+++ drawnings/serializers.py
@@ -1,7 +1,6 @@
 from rest_framework import serializers
 from drawnings.models import Drawning
 
-
 # Drawning serializer
 
 class DrawningSerializer(serializers.ModelSerializer):
