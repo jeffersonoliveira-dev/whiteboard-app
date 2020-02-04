f8fdb6e5fbb5f57b724378174df4887f4b29ed28
Jefferson Oliveira <cardbeatt@gmail.com>
2020-02-03 22:30:39 -0300

    fix: typescript and reducer included

--- frontend/src/routes/Login.tsx
+++ frontend/src/routes/Login.tsx
@@ -1,7 +1,4 @@
 import * as React from 'react'
-
-// form here to type and pass token
-
 export const Login = () => {
   return <h1> here is login </h1>
 }
--- frontend/src/store/index.ts
+++ frontend/src/store/index.ts
@@ -2,13 +2,12 @@ import { createStore, combineReducers } from 'redux'
 import { authReducer } from './user/reducers'
 
 const rootReducers = combineReducers({
-    authReducer
+  authReducer,
 })
 
 const InitialState = {}
 
-
-export const store = createStore(
-    rootReducers,
-    InitialState
-}
+export const store = createStore({
+  rootReducers,
+  InitialState,
+})
