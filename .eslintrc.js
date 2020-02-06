1bf399a0a310666910df929233f4f3d6b3a453e8
Jefferson Oliveira <cardbeatt@gmail.com>
2020-02-06 14:40:08 -0300

    starting with useContext and useReducer

--- .eslintrc.js
+++ .eslintrc.js
@@ -21,5 +21,7 @@ module.exports = {
     sourceType: 'module'
   },
   plugins: ['react', '@typescript-eslint', 'prettier'],
-  rules: {}
+  rules: {
+    'space-before-function-paren': 'off'
+  }
 }
