307bf5b93ea80e858943de38524732b34d357577
Jefferson Oliveira <cardbeatt@gmail.com>
2020-02-29 10:42:42 -0300

    basic routing finished

--- src/App.js
+++ src/App.js
@@ -1,8 +1,8 @@
 import React from "react";
 import { BrowserRouter as Router, Route } from "react-router-dom";
-import Home from "./routes/Home";
-import Login from "./routes/Login";
-import SignUp from "./routes/Login";
+import Home from "./routes/Home/index";
+import Login from "./routes/Login/index";
+import SignUp from "./routes/SignUp/index";
 import { GlobalStyles } from "./components/GlobalStyles";
 import Layout from "./components/Layout";
 
---
+++ src/routes/Login/index.js
@@ -0,0 +1,8 @@
+import React from "react";
+import { Container } from "./styles";
+
+const Login = () => {
+  return <Container>login component</Container>;
+};
+
+export default Login;
