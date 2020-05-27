3cb44083dbbd8344b4921e741f46ffa8dca79fdd
jefferson oliveira <cardbeatt@gmail.com>
2020-05-27 15:11:57 -0300

    sidebar mobile/pc

--- src/routes/Dashboard/SideBar.js
+++ src/routes/Dashboard/SideBar.js
@@ -1,3 +1,25 @@
 // sidebar  for pc and mobile
 // pc => 30% of the width
 // mobile => 100% on click with menu
+//
+
+import React, { useState } from "react";
+
+const SideBar = (props) => {
+  const [mobile, setMobile] = useState(false);
+
+  if (mobile) {
+    return (
+      <div>
+        <h1>mobile here</h1>
+      </div>
+    );
+  }
+  return (
+    <div>
+      <h1>pc master race</h1>
+    </div>
+  );
+};
+
+export default SideBar;
